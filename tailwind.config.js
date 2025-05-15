06fbb4d532fb2934facd3664bab7984fd6d05d35
Mahmud Preyom <preyom5076@example.com>
2025-05-15 20:03:02 +0600

    some bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
